abd60e7301dd40e6251cbbaff01f07c6e30103f4
Sebastian Herbermann <sebastian.herbermann@googlemail.com>
2012-04-23 18:11:10 +0200

    add clearfix after post

--- themes/modern/frontend/post/_post.php
+++ themes/modern/frontend/post/_post.php
@@ -5,3 +5,6 @@ use_helper('Shortcode');
 echo '<h1>'. link_to($post->getRaw('title'), 'post_show', $post) .'</h1>';
 $post_content = $post->getMarkdown() ? markdown( $post->getRaw('content') ) : $post->getRaw('content');
 echo do_shortcodes($post_content);
+
+?>
+<div class="clearfix"></div>
--- themes/modern/frontend/post/showSuccess.php
+++ themes/modern/frontend/post/showSuccess.php
@@ -14,6 +14,7 @@
 			echo do_shortcodes($post_content);
 		?>
 
+		<div class="clearfix"></div>
         <p class="post-meta"><small>
 
             <span class="label">
